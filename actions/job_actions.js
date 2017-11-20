0cbd2fd47c80c7198878809bd52fc2a288bab2bc
artokun <art.longbottom.jr@gmail.com>
2017-11-20 11:10:20 -0800

    map search works

--- actions/job_actions.js
+++ actions/job_actions.js
@@ -2,4 +2,25 @@ import axios from 'axios';
 import reverseGeocode from 'latlng-to-zip';
 import * as types from './types';
 
-export const fetchJobs = region => async dispatch => {};
+const JOB_ROOT_URL = 'http://api.indeed.com/ads/apisearch';
+const JOB_QUERY_PARAMS = {
+  publisher: '4201738803816157',
+  format: 'json',
+  v: '2',
+  latlong: 1,
+  radius: 10,
+  q: 'javascript',
+};
+
+export const fetchJobs = region => async dispatch => {
+  try {
+    const zip = await reverseGeocode(region);
+    let { data } = await axios.get(JOB_ROOT_URL, {
+      params: { ...JOB_QUERY_PARAMS, l: zip },
+    });
+    dispatch({ type: types.FETCH_JOBS, payload: data });
+    console.log(data);
+  } catch (err) {
+    console.error(err);
+  }
+};
--- components/JobsComponents/screens/MapScreen.js
+++ components/JobsComponents/screens/MapScreen.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../../../actions';
 import { View, Text, ActivityIndicator } from 'react-native';
+import { Button } from 'react-native-elements';
 import { MapView } from 'expo';
 import styled from 'styled-components/native';
 
@@ -14,17 +17,41 @@ class MapScreen extends Component {
     mapLoaded: false,
   };
 
+  componentDidMount() {
+    setTimeout(() => this.setState({ mapLoaded: true }), 300);
+  }
+
   onRegionChangeComplete = region => {
     this.setState({ region });
   };
 
+  onButtonPress = () => {
+    this.props.fetchJobs(this.state.region);
+  };
+
   render() {
+    if (!this.state.mapLoaded) {
+      return (
+        <View style={{ flex: 1, justifyContent: 'center' }}>
+          <ActivityIndicator size="large" />
+        </View>
+      );
+    }
     return (
       <MapScreenStyles>
         <MapViewStyles
           region={this.state.region}
           onRegionChangeComplete={this.onRegionChangeComplete}
         />
+        <ButtonContainerStyles>
+          <Button
+            large
+            title="Search This Area"
+            backgroundColor="#009688"
+            icon={{ name: 'search' }}
+            onPress={this.onButtonPress}
+          />
+        </ButtonContainerStyles>
       </MapScreenStyles>
     );
   }
@@ -32,5 +59,11 @@ class MapScreen extends Component {
 
 const MapScreenStyles = styled.View`flex: 1;`;
 const MapViewStyles = styled(MapView)`flex: 1;`;
+const ButtonContainerStyles = styled.View`
+  position: absolute;
+  bottom: 20;
+  left: 0;
+  right: 0;
+`;
 
-export default MapScreen;
+export default connect(null, actions)(MapScreen);
